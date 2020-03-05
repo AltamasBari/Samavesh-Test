2ec8cb80c1e3429b1067f997cc363be3b8605cb4
AltamasBari <altamasbari@gmail.com>
2020-03-05 16:16:46 +0530

    update2
    update2

--- js/main.js
+++ js/main.js
@@ -33,37 +33,6 @@ $(window).on('load', function() {
 		var bg = $(this).data('setbg');
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
-	
-	/*------------------
-		Hero Slider
-	--------------------*/
-	var $slider = $('.hero-slider');
-	var SLIDER_TIMEOUT = 10000;
-
-	$slider.owlCarousel({
-		items: 1,
-		nav: false,
-		dots: false,
-		autoplay: true,
-		autoplayTimeout: SLIDER_TIMEOUT,
-		animateOut: 'fadeOut',
-   		animateIn: 'fadeIn',
-		loop: true,
-		onInitialized: ({target}) => {
-			var animationStyle = '-webkit-animation-duration'+ SLIDER_TIMEOUT +'ms;animation-duration:'+ SLIDER_TIMEOUT+'ms';
-			var progressBar = $('<div class="slider-progress-bar"><span class="progress" style='+ animationStyle +'></span></div>');
-			$(target).append(progressBar);
-		},
-		onChanged: ({type, target}) => {
-			if (type === 'changed') {
-				var $progressBar = $(target).find('.slider-progress-bar');
-				var clonedProgressBar = $progressBar.clone(true);
-
-				$progressBar.remove();
-				$(target).append(clonedProgressBar);
-			}
-		}
-	});
 
 	/*------------------
 		Video Popup
@@ -102,22 +71,4 @@ $(window).on('load', function() {
 		}
 	});
 
-
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
 })(jQuery);
