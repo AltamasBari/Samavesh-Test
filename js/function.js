2e7a3c5d3de456ec8dc4070e4bbd79d04bf538e5
AITIK DANDAPAT <46849982+i-tick@users.noreply.github.com>
2020-03-06 14:45:27 +0530

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,26 +1,28 @@
-const team_leader1 = document.getElementById('team_leader1');
-const team_leader2 = document.getElementById('team_leader2');
-const email1 = document.getElementById('email1');
-const email2 = document.getElementById('email2');
-const phone1 = document.getElementById('phone1');
-const phone2 = document.getElementById('phone2');
-const college1 = document.getElementById('college1');
-const college2 = document.getElementById('college2');
-const member11 = document.getElementById('member11');
-const member21 = document.getElementById('member21');
-const member31 = document.getElementById('member31');
-const member12 = document.getElementById('member12');
-const member22 = document.getElementById('member22');
-const member32 = document.getElementById('member32');
+var team_leader1 = document.getElementById('team_leader1');
+var team_leader2 = document.getElementById('team_leader2');
+var email1 = document.getElementById('email1');
+var email2 = document.getElementById('email2');
+var phone1 = document.getElementById('phone1');
+var phone2 = document.getElementById('phone2');
+var college1 = document.getElementById('college1');
+var college2 = document.getElementById('college2');
+var member11 = document.getElementById('member11');
+var member21 = document.getElementById('member21');
+var member31 = document.getElementById('member31');
+var member12 = document.getElementById('member12');
+var member22 = document.getElementById('member22');
+var member32 = document.getElementById('member32');
 
 
 const sub1 = document.getElementById('submit_btn1');
 const sub2 = document.getElementById('submit_btn2');
 
 const database = firebase.database();
+const rootref1 = database.ref('MAZE MICE');
 sub1.addEventListener('click',(e)=>{
 	e.preventDefault();
-	database.ref('/MAZE MICE/'+ email1.value).set({
+	const autoid1 = rootref1.push().key
+	rootref1.child(autoid1).set({
 		team_leader: team_leader1.value,
 		email: email1.value,
 		phone: phone1.value,
@@ -31,10 +33,11 @@ sub1.addEventListener('click',(e)=>{
 	});
 });
 
-
+const rootref2 = database.ref('Devil Valley');
 sub2.addEventListener('click',(e)=>{
 	e.preventDefault();
-	database.ref('/DEVIL,S VALLEY/'+ email2.value).set({
+	const autoid2 = rootref2.push().key
+	rootref2.child(autoid2).set({
 		team_leader: team_leader2.value,
 		email: email2.value,
 		phone: phone2.value,
